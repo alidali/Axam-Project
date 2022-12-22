c1d3331e6441a42b15a3079522c7762fec8e7d85
inesdali <daliines96@gmail.com>
2022-12-22 11:59:56 +0100

    change-login

---
+++ pages/logo.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+
+const Logo = () => {
+	return (
+		<>
+
+			<div>
+				<Image
+                src="/assets/images/banners/logo.png"
+                height="100"
+                width="190"
+			       />
+			</div>
+
+
+		</>
+	);
+};
+
+export default Logo;
